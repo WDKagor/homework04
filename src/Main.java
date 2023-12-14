c380e8bba978459ff91c0a2dfef45a822802ef2a
briz1 <my1queen8my1king@gmail.com>
2023-12-14 17:06:42 +0700

    Two tasks are complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println ("Задача 1");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println ("Если возраст человека равен " + age + " лет, он совершеннолетний");}
+        else System.out.println("Возраст равен " + age + " лет, человек не достиг совершенолетия, нужно подождать");
+
+        System.out.println ("   ");
+        System.out.println ("Задача 2");
+        int temp = 4;
+        if (temp < 5) {System.out.println("На улице холодно, нужно надеть шапку");
+        }
+        else System.out.println("Сегодня тепло, можно идти без шапки");
+
+
     }
 }
