b257e3a6e7d9cd62cf113138a6d1ddefb1b86af2
briz1 <my1queen8my1king@gmail.com>
2023-12-14 19:12:30 +0700

    Two tasks are complete

--- src/Main.java
+++ src/Main.java
@@ -24,10 +24,32 @@ public class Main {
         System.out.println ("   ");
         System.out.println ("Задача 4");
         int age2 = 15;
-        if (age2 > 2 && age2 < 6) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему нужно ходить в детский сад");}
-        if (age2 > 7 && age2 < 17) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему нужно ходить в школу");}
-        if (age2 > 18 && age2 < 24) {System.out.println("Если возраст человека равен " + age2 + " лет, то его место в университете");}
-        if (age2 > 24) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему пора ходить на работу");}
+        if (age2 > 2 && age2 <= 6) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему нужно ходить в детский сад");}
+        else if (age2 >= 7 && age2 <= 17) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему нужно ходить в школу");}
+        else if (age2 >= 18 && age2 < 24) {System.out.println("Если возраст человека равен " + age2 + " лет, то его место в университете");}
+        else if (age2 >= 24) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему пора ходить на работу");}
+
+        System.out.println ("   ");
+        System.out.println ("Задача 5");
+        int ageChild = 5;
+        if (ageChild < 5) {System.out.println("Если возраст ребенка равен " + ageChild + " лет, то он не может кататься на аттракционе");}
+        if (ageChild >= 5 && ageChild < 14) {System.out.println("Если возраст ребенка равен " + ageChild + " лет, то он может кататься только в сопровождении взрослого");}
+        if (ageChild >= 14) {System.out.println("Если возраст ребенка равен " + ageChild + " лет, то он может кататься без сопровождения взрослого");}
+
+        System.out.println ("   ");
+        System.out.println ("Задача 6");
+        int numberOfPeopleInTheCarriage = 47;
+        if (numberOfPeopleInTheCarriage >= 60 && numberOfPeopleInTheCarriage < 102) {System.out.println("В вагоне свободно только " + (102 - numberOfPeopleInTheCarriage) + " стоячих мест");}
+        else if (numberOfPeopleInTheCarriage < 60 && numberOfPeopleInTheCarriage < 102) {System.out.println("В вагоне свободно " + (60 - numberOfPeopleInTheCarriage) + " сидячих мест и " + (102 - 60) + " стоячих места");}
+        else if (numberOfPeopleInTheCarriage == 102) {System.out.println ("В вагоне нет свободных мест");};
+
+        System.out.println ("   ");
+        System.out.println ("Задача 7");
+
+
+
+
+
 
 
 
