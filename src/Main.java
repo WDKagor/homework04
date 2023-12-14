cc4a3fa0ae807fc47ab641eb38afaade47eb7427
briz1 <my1queen8my1king@gmail.com>
2023-12-14 17:41:10 +0700

    Four tasks are complete

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,24 @@ public class Main {
         }
         else System.out.println("Сегодня тепло, можно идти без шапки");
 
+        System.out.println ("   ");
+        System.out.println ("Задача 3");
+        int speed = 80;
+        if (speed <= 60) {
+            System.out.println("Если скорость " + speed + " км/ч, то можно спокойно ехать");
+        }
+        else System.out.println("Если скорость " + speed + " км/ч, то придется заплатить штаф");
+
+        System.out.println ("   ");
+        System.out.println ("Задача 4");
+        int age2 = 15;
+        if (age2 > 2 && age2 < 6) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему нужно ходить в детский сад");}
+        if (age2 > 7 && age2 < 17) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему нужно ходить в школу");}
+        if (age2 > 18 && age2 < 24) {System.out.println("Если возраст человека равен " + age2 + " лет, то его место в университете");}
+        if (age2 > 24) {System.out.println("Если возраст человека равен " + age2 + " лет, то ему пора ходить на работу");}
+
+
+
 
     }
 }
